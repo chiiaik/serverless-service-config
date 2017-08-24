200d33b5077d40487eb809d71b6154ddaed29b3f
Chii Aik Fang <chiiaik@tinkerstuff.com>
2017-08-24 19:46:14 +0800

    Fix typo

--- service-config.js
+++ service-config.js
@@ -2,7 +2,7 @@
  * @Author: Chii Aik Fang 
  * @Date: 2017-08-07 15:08:20 
  * @Last Modified by: Chii Aik Fang
- * @Last Modified time: 2017-08-24 19:44:45
+ * @Last Modified time: 2017-08-24 19:46:02
  */
 const _ = require('lodash');
 const fs = require('fs');
@@ -30,7 +30,7 @@ class ServiceConfig {
         throw new Error('customerId command line option is required');
     }
     if (_.isEmpty(this.options.apiKey)) {
-        throw new Error('apiKye command line option is required');
+        throw new Error('apiKey command line option is required');
     }
     console.log('options:', JSON.stringify(this.options));
     let region = this.options.region ? this.options.region : this.serverless.service.provider.region;
