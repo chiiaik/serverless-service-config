bf225ca22c72149f083d6e593466e843a3fb2d76
Chii Aik Fang <chiiaik@tinkerstuff.com>
2017-08-25 15:38:47 +0800

    Remove old codes to read configuration from a file

--- service-config.js
+++ service-config.js
@@ -2,7 +2,7 @@
  * @Author: Chii Aik Fang 
  * @Date: 2017-08-07 15:08:20 
  * @Last Modified by: Chii Aik Fang
- * @Last Modified time: 2017-08-25 11:18:37
+ * @Last Modified time: 2017-08-25 15:38:05
  */
 const _ = require('lodash');
 const fs = require('fs');
@@ -44,48 +44,20 @@ class ServiceConfig {
 
   beforeDeployFunctions() {
     console.log('Before deploying functions');
-    // console.log('options:', this.options);
-    // if (_.isEmpty(this.options.config)) {
-    //   throw new Error('Please specify the file path to your config file with command line switch --config');
-    // }
-    // let content = fs.readFileSync(this.options.config, 'utf8');
-    // if (_.isEmpty(content)) {
-    //   throw new Error('Problem reading config file at ', this.options.config);
-    // }
-    // let config;
-    // try {
-    //   config = JSON.parse(content);
-    // } catch (e) {
-    //   throw new Error('Problem parsing config file due to ' + e);
-    // }
-    // if (_.isEmpty(config)) {
-    //   throw new Error('Please specify customer and accountId in your config');
-    // }
-    // if (_.isEmpty(config.customer)) {
-    //   throw new Error('Please specify customer in your config');
-    // }
-    // if (!_.isString(config.accountId)) {
-    //   throw new Error('Please specify accountId in string format in your config');
-    // }
-    // if (!_.isEmpty(config.region)) {
-    //   throw new Error('Please specify region, for example, us-east-1 in your config');
-    // }
-    // if (!_.isEmpty(config.stage)) {
-    //   throw new Error('Please specify stage, for example, dev in your config');
-    // }
-    // // let region = this.options.region ? this.options.region : this.serverless.service.provider.region;
-    // // let stage = this.options.stage ? this.options.stage : this.serverless.service.provider.stage;
-    // // config.region = region;
-    // // config.stage = stage;
-    // console.log(config);
-    // this.serverless.service.custom = config;
-    // let configPath = path.join(this.serverless.config.servicePath, 'config.json');
-    // console.log('config file:', configPath);
-    // fs.writeFile(configPath, JSON.stringify(config), function(error) {
-    //     if (error) {
-    //         console.error('Problem creating service config file at', configPath);
-    //     }
-    // });
+    let region = this.serverless.service.provider.region;
+    let stage = this.serverless.service.provider.stage;
+    let config = {
+      region: region,
+      stage: stage,
+    };
+    console.log('config:', config);
+    let configPath = path.join(this.serverless.config.servicePath, 'config.json');
+    console.log('config file:', configPath);
+    fs.writeFile(configPath, JSON.stringify(config), function(error) {
+        if (error) {
+            console.error('Problem creating service config file at', configPath);
+        }
+    });
   }
 
   deployFucntions() {
